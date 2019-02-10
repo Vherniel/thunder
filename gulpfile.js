8029f1f99c1f7795d3a1939e2999d769ad93cf1d
Vherniel <vherniel11@gmail.com>
2019-02-11 03:38:25 +0800

    Update

--- gulpfile.js
+++
@@ -1,9 +0,0 @@
-// const gulp = require('gulp');
-// const sass = require('sass');
-// const pug = require('pug');
-
-// gulp.task('sass', function() {
-//     gulp.src('src/sass/*.scss')
-//         .pipe(sass().on('error', sass.logError))
-//         .pipe(gulp.dest('build/css'))
-// });
